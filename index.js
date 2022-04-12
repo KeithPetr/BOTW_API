4940de09c1adc8248955e3c1b2ab5625bed0b203
KeithPetr <keith.petryshyn@gmail.com>
2022-04-12 20:38:01 -0300

    displaying food items under the creature tab in a grid

---
+++ index.js
@@ -0,0 +1,19 @@
+const creatures = document.getElementById("creatures")
+const images = document.getElementById("image-container")
+
+function getCreatureData() {
+    fetch("https://botw-compendium.herokuapp.com/api/v2")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.data.creatures.food)
+        for (let i = 0; i < data.data.creatures.food.length; i++) {
+            const img = document.createElement("img")
+            img.src = data.data.creatures.food[i].image
+            images.appendChild(img)
+        }
+    })
+}
+
+creatures.addEventListener("click", getCreatureData)
+
+
