c9875adf9e2538ce0c5a5dcc64dc0ab1e9173a3a
KeithPetr <keith.petryshyn@gmail.com>
2022-04-12 21:13:20 -0300

    added scrollbar to image overflow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getCreatureData() {
     fetch("https://botw-compendium.herokuapp.com/api/v2")
     .then(response => response.json())
     .then(data => {
-        console.log(data.data.creatures.food)
+        console.log(data.data.creatures)
         for (let i = 0; i < data.data.creatures.food.length; i++) {
             const img = document.createElement("img")
             img.src = data.data.creatures.food[i].image
