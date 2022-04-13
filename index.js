78218be7b8a57c915040fb9d656256f1a7576300
KeithPetr <keith.petryshyn@gmail.com>
2022-04-13 10:43:20 -0300

    added event.target.src to make small images clickable

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const creatures = document.getElementById("creatures")
 const images = document.getElementById("image-container")
+const largeImage = document.getElementById("large-image")
 
-function getCreatureData() {
-    fetch("https://botw-compendium.herokuapp.com/api/v2")
+let img
+
+function getSmallCreatureData() {
+    fetch(`https://botw-compendium.herokuapp.com/api/v2/category/creatures`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.data.creatures)
-        for (let i = 0; i < data.data.creatures.food.length; i++) {
-            const img = document.createElement("img")
-            img.src = data.data.creatures.food[i].image
+        console.log(data.data.food)
+        for (let i = 0; i < data.data.food.length; i++) {
+            img = document.createElement("img")
+            img.src = data.data.food[i].image
+            img.id = data.data.food[i].id
+            img.addEventListener("click", function(event) {
+                console.log("clicked")
+                console.log(event)
+                largeImage.src = event.target.src
+            })
             images.appendChild(img)
+            console.log(img)
         }
-    })
+    }) 
 }
 
-creatures.addEventListener("click", getCreatureData)
 
+
+creatures.addEventListener("click", getSmallCreatureData)
 
